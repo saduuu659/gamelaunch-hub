84635c2a89a7d77c78d28432e94675c03d59fd7c
saduuu659 <homefly1215@gmail.com>
2026-05-17 20:38:00 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Simple search filter (basic version)
+const searchInput = document.getElementById("search");
+
+if (searchInput) {
+  searchInput.addEventListener("keyup", function () {
+    let input = searchInput.value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+      let text = cards[i].innerText.toLowerCase();
+
+      if (text.includes(input)) {
+        cards[i].style.display = "block";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  });
+}
